8e2774739abb1ef96c55ecd5d375231c9c1ae793
Rouzbeh <47076354+Rouzbeh99@users.noreply.github.com>
2022-06-20 11:53:38 -0600

    Update iterativeALC.c

--- ALC Examples/Example 1/iterativeALC.c
+++ ALC Examples/Example 1/iterativeALC.c
@@ -96,7 +96,7 @@ void ACLEVersion(int *a, int *b, int *c, bool *cond, int n) {
 int main() {
     int a[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
     int b[] = {17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
-    bool cond[] = {true, false, true, false, true, false, true, false, true, fa >
+    bool cond[] = {true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false};
             int c[] = { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 };
             int n = 16;
 
