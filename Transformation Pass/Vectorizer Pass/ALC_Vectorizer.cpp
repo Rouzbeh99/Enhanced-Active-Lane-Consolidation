612b5402d6edd042746468fdd312c7672cf21636
Rouzbeh99 <rouzbehpaktinat99@gmail.com>
2022-07-20 14:53:25 -0600

    Finish implementing unroll phase

--- Transformation Pass/Vectorizer Pass/ALC_Vectorizer.cpp
+++ Transformation Pass/Vectorizer Pass/ALC_Vectorizer.cpp
@@ -28,11 +28,16 @@ namespace {
     void removeLastInstruction(BasicBlock *BB);
 
 
-    void refineCFG(std::vector<BasicBlock *> *newBlocks, BasicBlock *header, BasicBlock *latch);
+    void refineCFG(std::vector<BasicBlock *> *newBlocks, BasicBlock *header, BasicBlock *latch, BasicBlock *thenBlock,
+                   Loop *L);
 
     void printVMap(ValueToValueMapTy *VMap);
 
     void printLoop(BasicBlock *header);
+
+    BasicBlock *findAndRefineThenBlock(BasicBlock *header, BasicBlock *latch);
+
+    Value *findInductionVariableInLatch(BasicBlock *latch);
 
 
 
@@ -73,12 +78,14 @@ namespace {
 
         BasicBlock *latch = L->getLoopLatch();
 
+        BasicBlock *thenBlock = findAndRefineThenBlock(header, latch);
+
 
         checkCondition(header);
 
         std::vector<BasicBlock *> *newBlocks = doUnrolling(header, latch, L, 3);
 
-        refineCFG(newBlocks, header, latch);
+        refineCFG(newBlocks, header, latch, thenBlock, L);
 
         printLoop(header);
 
@@ -128,7 +135,7 @@ namespace {
             SmallVector<BasicBlock *, 2> newlyGeneratedBlock;
             ValueToValueMapTy VMap;
 
-            //////////////////////////////  ADD HEADER /////////////////////////////////////////////////
+            //////////////////////////////  ADD A HEADER /////////////////////////////////////////////////
             auto newBB = CloneBasicBlock(prevHeader, VMap);
             header->getParent()->getBasicBlockList().insert(blockInsertPt, newBB);
 
@@ -152,7 +159,7 @@ namespace {
 
 
             prevHeader = newBB;
-            ///////////////////////////////// ADD LATCH //////////////////////////////////////////////
+            ///////////////////////////////// ADD A LATCH //////////////////////////////////////////////
 
             newBB = CloneBasicBlock(prevLatch, VMap);
             header->getParent()->getBasicBlockList().insert(blockInsertPt, newBB);
@@ -172,11 +179,11 @@ namespace {
 
         }
 
-        llvm::outs() << "-------------------------------------------------------------" << "\n";
         return newBlocks;
     }
 
-    void refineCFG(std::vector<BasicBlock *> *newBlocks, BasicBlock *header, BasicBlock *latch) {
+    void refineCFG(std::vector<BasicBlock *> *newBlocks, BasicBlock *header, BasicBlock *latch, BasicBlock *thenBlock,
+                   Loop *L) {
 
         // blocks that used to branch to latch should now branch to header
 
@@ -188,7 +195,16 @@ namespace {
         }
         llvm::BranchInst::Create(latch, header);
 
+
+
         //then latch branches to the first generated block
+        // this also removes phi node in the header so first keep track of induction variable uses in header
+        auto *inductionVariable = dyn_cast<PHINode>(&header->getInstList().front());
+        std::vector<Value *> inductionVariableUses;
+        for (Use &use: inductionVariable->operands()) {
+            inductionVariableUses.push_back(use.get());
+        }
+
         for (auto succ: successors(latch)) {
             succ->removePredecessor(latch);
         }
@@ -201,25 +217,89 @@ namespace {
 
             // last block should have its branch instruction
             if ((*it) != newBlocks->back()) {
+
                 for (auto succ: successors(*it)) {
+
+                    if (!L->contains(*(it))) {
+                        continue;
+                    }
+
+                    if (it == newBlocks->begin() + 1) {
+                        succ->print(outs());
+                    }
                     succ->removePredecessor(*it);
                 }
+
                 (*it)->getTerminator()->eraseFromParent();
                 llvm::BranchInst::Create(*(it + 1), *it);
+            } else {
+
+                // for the last block, the edge should point to then block
+                auto *branchInstr = dyn_cast<BranchInst>((*it)->getTerminator());
+                if (branchInstr->isConditional()) {
+                    for (int i = 1; i < 3; ++i) {
+                        Value *label = branchInstr->getOperand(i);
+                        auto *block = dyn_cast<BasicBlock>(label);
+                        if (block == header) {
+                            // make it point to the first block after loop header instead of prev header
+                            branchInstr->setOperand(i, dyn_cast<Value>(thenBlock));
+                        }
+                    }
+
+                }
             }
 
         }
 
+
+
         // Finally, blocks (then blocks) that used to branch to latch should now branch to header
         for (auto BB: predecessors(latch)) {
-            if (BB == header) {
+            if (BB == header || !L->contains(BB)) {
                 continue;
             }
             BB->getTerminator()->replaceSuccessorWith(latch, header);
         }
 
+        // add phi node in the beginning of the header block
+        PHINode *phiNode = llvm::PHINode::Create(llvm::IntegerType::getInt64Ty(header->getContext()), 2);
+        header->getInstList().insert(header->getFirstInsertionPt(), phiNode);
 
-        int count = 0;
+        for (auto pred: predecessors(header)) {
+            if (!L->contains(pred)) {
+                // it's preHeader, the coming value should be zero
+                phiNode->addIncoming(llvm::ConstantInt::get(header->getContext(), llvm::APInt(64, 0, false)), pred);
+            } else {
+                // it's new latch, the coming value should be loopInductionVariable
+                phiNode->addIncoming(findInductionVariableInLatch(newBlocks->back()), pred);
+            }
+
+        }
+
+        // map instructions in the header and latch to new induction variable
+        for (auto &instr: header->getInstList()) {
+            for (auto value: inductionVariableUses) {
+                if (instr.getOperand(0) == value) {
+                    instr.setOperand(0, phiNode);
+                } else if (dyn_cast<BinaryOperator>(&instr)) {
+                    if (instr.getOperand(1) == value) {
+                        instr.setOperand(1, phiNode);
+                    }
+                }
+            }
+        }
+
+        for (auto &instr: latch->getInstList()) {
+            for (auto value: inductionVariableUses) {
+                if (instr.getOperand(0) == value) {
+                    instr.setOperand(0, phiNode);
+                } else if (dyn_cast<BinaryOperator>(&instr)) {
+                    if (instr.getOperand(1) == value) {
+                        instr.setOperand(1, phiNode);
+                    }
+                }
+            }
+        }
 
 
     }
@@ -248,6 +328,38 @@ namespace {
         }
 
         llvm::outs() << "\n";
+    }
+
+    BasicBlock *findAndRefineThenBlock(BasicBlock *header, BasicBlock *latch) {
+        BasicBlock *thenBlock;
+        for (auto BB: successors(header)) {
+            if (BB != latch) {
+                thenBlock = BB;
+            }
+        }
+
+        for (auto succ: successors(thenBlock)) {
+            succ->removePredecessor(thenBlock);
+        }
+
+        thenBlock->getTerminator()->eraseFromParent();
+        llvm::BranchInst::Create(header, thenBlock);
+
+
+        return thenBlock;
+    }
+
+    // TODO: add the check in analysis pass to check if induction variable is added by 1
+    Value *findInductionVariableInLatch(BasicBlock *latch) {
+        for (auto &instr: latch->getInstList()) {
+            if (instr.getOpcode() == Instruction::Add) {
+                if (auto *CI = dyn_cast<ConstantInt>(instr.getOperand(1)))
+                    if (CI->isOne()) {
+                        return dyn_cast<Value>(&instr);
+                    }
+            }
+        }
+        return nullptr;
     }
 
 
