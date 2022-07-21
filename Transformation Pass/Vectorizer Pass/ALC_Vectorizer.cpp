e4852d34a3f25e910b967d543b423d8b74d8d59f
Rouzbeh99 <rouzbehpaktinat99@gmail.com>
2022-07-21 13:17:18 -0600

    Eliminate redundant instructions and fix bug in refineCFG function, remapping new phi node

--- Transformation Pass/Vectorizer Pass/ALC_Vectorizer.cpp
+++ Transformation Pass/Vectorizer Pass/ALC_Vectorizer.cpp
@@ -6,6 +6,7 @@
 #include "llvm/IR/ValueMap.h"
 #include "llvm/Transforms/Utils/Cloning.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "map"
 
 using namespace llvm;
 
@@ -25,19 +26,20 @@ namespace {
 
     std::vector<BasicBlock *> *doUnrolling(BasicBlock *header, BasicBlock *latch, Loop *L, int unrollFactor);
 
-    void removeLastInstruction(BasicBlock *BB);
-
-
     void refineCFG(std::vector<BasicBlock *> *newBlocks, BasicBlock *header, BasicBlock *latch, BasicBlock *thenBlock,
                    Loop *L);
-
-    void printVMap(ValueToValueMapTy *VMap);
 
     void printLoop(BasicBlock *header);
 
     BasicBlock *findAndRefineThenBlock(BasicBlock *header, BasicBlock *latch);
 
     Value *findInductionVariableInLatch(BasicBlock *latch);
+
+    void removeRedundantInstructions(BasicBlock *header);
+
+    void
+    mapNewPhiNodeInstructions(BasicBlock *BB, const std::map<Instruction *, int> &inductionVariableUsers,
+                              PHINode *phiNode);
 
 
 
@@ -86,6 +88,8 @@ namespace {
         std::vector<BasicBlock *> *newBlocks = doUnrolling(header, latch, L, 3);
 
         refineCFG(newBlocks, header, latch, thenBlock, L);
+
+        removeRedundantInstructions(header);
 
         printLoop(header);
 
@@ -136,7 +140,7 @@ namespace {
             ValueToValueMapTy VMap;
 
             //////////////////////////////  ADD A HEADER /////////////////////////////////////////////////
-            auto newBB = CloneBasicBlock(prevHeader, VMap);
+            auto newBB = CloneBasicBlock(prevHeader, VMap, ".headerCopy");
             header->getParent()->getBasicBlockList().insert(blockInsertPt, newBB);
 
             newlyGeneratedBlock.push_back(newBB);
@@ -161,7 +165,7 @@ namespace {
             prevHeader = newBB;
             ///////////////////////////////// ADD A LATCH //////////////////////////////////////////////
 
-            newBB = CloneBasicBlock(prevLatch, VMap);
+            newBB = CloneBasicBlock(prevLatch, VMap, ".latchCopy");
             header->getParent()->getBasicBlockList().insert(blockInsertPt, newBB);
             newlyGeneratedBlock.push_back(newBB);
             newBlocks->push_back(newBB);
@@ -200,10 +204,19 @@ namespace {
         //then latch branches to the first generated block
         // this also removes phi node in the header so first keep track of induction variable uses in header
         auto *inductionVariable = dyn_cast<PHINode>(&header->getInstList().front());
-        std::vector<Value *> inductionVariableUses;
-        for (Use &use: inductionVariable->operands()) {
-            inductionVariableUses.push_back(use.get());
+        std::map<Instruction *, int> inductionVariableUsers;
+        for (auto user: inductionVariable->users()) {
+            if (isa<Instruction>(user)) {
+                auto *instr = dyn_cast<Instruction>(user);
+                for (int i = 0; i < instr->getNumOperands(); ++i) {
+                    if (instr->getOperand(i) == inductionVariable) {
+                        inductionVariableUsers.insert({instr, i});
+                    }
+                }
+            }
+
         }
+
 
         for (auto succ: successors(latch)) {
             succ->removePredecessor(latch);
@@ -222,10 +235,6 @@ namespace {
 
                     if (!L->contains(*(it))) {
                         continue;
-                    }
-
-                    if (it == newBlocks->begin() + 1) {
-                        succ->print(outs());
                     }
                     succ->removePredecessor(*it);
                 }
@@ -252,7 +261,6 @@ namespace {
         }
 
 
-
         // Finally, blocks (then blocks) that used to branch to latch should now branch to header
         for (auto BB: predecessors(latch)) {
             if (BB == header || !L->contains(BB)) {
@@ -276,48 +284,13 @@ namespace {
 
         }
 
-        // map instructions in the header and latch to new induction variable
-        for (auto &instr: header->getInstList()) {
-            for (auto value: inductionVariableUses) {
-                if (instr.getOperand(0) == value) {
-                    instr.setOperand(0, phiNode);
-                } else if (dyn_cast<BinaryOperator>(&instr)) {
-                    if (instr.getOperand(1) == value) {
-                        instr.setOperand(1, phiNode);
-                    }
-                }
-            }
-        }
-
-        for (auto &instr: latch->getInstList()) {
-            for (auto value: inductionVariableUses) {
-                if (instr.getOperand(0) == value) {
-                    instr.setOperand(0, phiNode);
-                } else if (dyn_cast<BinaryOperator>(&instr)) {
-                    if (instr.getOperand(1) == value) {
-                        instr.setOperand(1, phiNode);
-                    }
-                }
-            }
-        }
-
-
+        // map instructions in the header, latch and thenBlock to new induction variable
+        mapNewPhiNodeInstructions(header, inductionVariableUsers, phiNode);
+        mapNewPhiNodeInstructions(latch, inductionVariableUsers, phiNode);
+        mapNewPhiNodeInstructions(thenBlock, inductionVariableUsers, phiNode);
+        //TODO: remapping should be applied to other blocks as well
     }
 
-
-    void printVMap(ValueToValueMapTy *VMap) {
-        llvm::outs() << "------------------------------------------------------------------------------------------ \n";
-        for (auto pair: *VMap) {
-            if (isa<DbgInfoIntrinsic>(pair.first)) {
-                continue;
-            }
-            pair.first->print(outs());
-            llvm::outs() << " ----------------> ";
-            pair.second->print(outs());
-            llvm::outs() << "\n";
-        }
-        llvm::outs() << "------------------------------------------------------------------------------------------ \n";
-    }
 
     void printLoop(BasicBlock *header) {
 
@@ -360,6 +333,52 @@ namespace {
             }
         }
         return nullptr;
+    }
+
+    void removeRedundantInstructions(BasicBlock *header) {
+        BasicBlock *BB = header->getNextNode(); // start from the next node of the header
+        while (BB) {
+
+            // values in headerCopy blocks will be used for predicates later
+            if (BB->getName().contains("headerCopy")) {
+                BB = BB->getNextNode();
+                continue;
+            }
+
+            for (auto &instr: make_early_inc_range(*BB)) {  // need to be able to remove while iterating
+                if (instr.getNumUses() == 0) {
+
+                    // TODO: complete the list
+                    if (isa<BranchInst>(instr) || isa<DbgInfoIntrinsic>(instr) || isa<StoreInst>(instr) ||
+                        instr.mayHaveSideEffects() || isa<LandingPadInst>(instr)) {
+                        continue;
+                    } else {
+                        instr.dropAllReferences();
+                        instr.removeFromParent();
+                    }
+
+                }
+
+            }
+
+            BB = BB->getNextNode();
+        }
+    }
+
+    void
+    mapNewPhiNodeInstructions(BasicBlock *BB, const std::map<Instruction *, int> &inductionVariableUsers,
+                              PHINode *phiNode) {
+        for (auto &instr: BB->getInstList()) {
+            for (auto pair: inductionVariableUsers) {
+                for (int i = 0; i < instr.getNumOperands(); ++i) {
+                    Instruction *retrievedInstruction = pair.first;
+                    if (&instr == retrievedInstruction) {
+                        instr.setOperand(pair.second, phiNode);
+                    }
+
+                }
+            }
+        }
     }
 
 
