c4ffb1a038544287913a42f3a8a66b157ff92933
João P. L. de Carvalho <jaopaulolc@gmail.com>
2022-09-23 09:55:13 -0600

    Add gitignore and untrack generated files

--- Transformation Pass/Vectorizer Pass/IntrinsicCallGenerator/build/CMakeFiles/install-libLLVMIntrinsicCallGenerator-stripped.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/install-libLLVMIntrinsicCallGenerator-stripped"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/install-libLLVMIntrinsicCallGenerator-stripped.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
