8ff9ae0c984ca5d6428ac5f42ac2daf2316abb7f
João P. L. de Carvalho <jaopaulolc@gmail.com>
2022-10-04 17:43:15 -0600

    Run clang-format

--- Transformation-Pass/Vectorizer-Pass/IntrinsicCallGenerator/IntrinsicCallGenerator.cpp
+++ Transformation-Pass/Vectorizer-Pass/IntrinsicCallGenerator/IntrinsicCallGenerator.cpp
@@ -2,103 +2,117 @@
 #include "llvm/IR/IntrinsicsAArch64.h"
 
 Value *IntrinsicCallGenerator::createAllTruePredicates(IRBuilder<> &IRB) {
-    auto *VecTy = VectorType::get(IRB.getInt1Ty(), VF, /*Scalable*/ true);
-    auto *constantInt = llvm::ConstantInt::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_ptrue, {VecTy}, {constantInt});
+  auto *VecTy = VectorType::get(IRB.getInt1Ty(), VF, /*Scalable*/ true);
+  auto *constantInt =
+      llvm::ConstantInt::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_ptrue, {VecTy},
+                             {constantInt});
 }
 
+Value *IntrinsicCallGenerator::createCompactInstruction(
+    IRBuilder<> &IRB, Value *toBeCompacted, Value *predicatedVector) {
+  auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_compact, {VecTy},
+                             {predicatedVector, toBeCompacted});
+}
+
+Value *IntrinsicCallGenerator::createCntpInstruction(IRBuilder<> &IRB,
+                                                     Value *elements,
+                                                     Value *predicatedVector) {
+  auto *VecTy = VectorType::get(IRB.getInt1Ty(), VF, true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_cntp, {VecTy},
+                             {predicatedVector, elements});
+}
+
+Value *IntrinsicCallGenerator::createWhileltInstruction(IRBuilder<> &IRB,
+                                                        Value *LHSOp,
+                                                        Value *RHSOp) {
+  auto *VecTy = VectorType::get(IRB.getInt1Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_whilelt,
+                             {VecTy, IRB.getInt64Ty()}, {LHSOp, RHSOp});
+}
 
 Value *
-IntrinsicCallGenerator::createCompactInstruction(IRBuilder<> &IRB, Value *toBeCompacted,
-                                                 Value *predicatedVector) {
-    auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_compact, {VecTy} , {predicatedVector, toBeCompacted});
+IntrinsicCallGenerator::createSpliceInstruction(IRBuilder<> &IRB,
+                                                Value *firstOp, Value *secondOp,
+                                                Value *predicatedVector) {
+  auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_splice,
+                             {VecTy, IRB.getInt64Ty()},
+                             {predicatedVector, firstOp, secondOp});
 }
 
-Value *
-IntrinsicCallGenerator::createCntpInstruction(IRBuilder<> &IRB, Value *elements, Value *predicatedVector) {
-    auto *VecTy = VectorType::get(IRB.getInt1Ty(), VF, true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_cntp, {VecTy} , {predicatedVector, elements});
+Value *IntrinsicCallGenerator::createSelInstruction(IRBuilder<> &IRB,
+                                                    Value *firstOp,
+                                                    Value *secondOp,
+                                                    Value *predicatedVector) {
+  auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_sel, {VecTy},
+                             {predicatedVector, firstOp, secondOp});
 }
 
-
-Value *
-IntrinsicCallGenerator::createWhileltInstruction(IRBuilder<> &IRB, Value *LHSOp, Value *RHSOp) {
-    auto *VecTy = VectorType::get(IRB.getInt1Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_whilelt, {VecTy, IRB.getInt64Ty()} , {LHSOp, RHSOp});
+Value *IntrinsicCallGenerator::createIndexInstruction(IRBuilder<> &IRB,
+                                                      Value *firstOp,
+                                                      Value *secondOp) {
+  auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_index, {VecTy},
+                             {firstOp, secondOp});
 }
 
-Value *IntrinsicCallGenerator::createSpliceInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp,
-                                                          Value *predicatedVector) {
-    auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_splice, {VecTy, IRB.getInt64Ty()} , {predicatedVector, firstOp, secondOp});
-
+Value *IntrinsicCallGenerator::createGatherLoadInstruction(
+    IRBuilder<> &IRB, Value *ptr, Value *predicatedVector, Value *indices) {
+  auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_ld1_gather_sxtw_index,
+                             {VecTy}, {predicatedVector, ptr, indices});
 }
 
-Value *IntrinsicCallGenerator::createSelInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp,
-                                                       Value *predicatedVector) {
-    auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_sel, {VecTy} , {predicatedVector, firstOp, secondOp});
-
-}
-
-Value *IntrinsicCallGenerator::createIndexInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp) {
-    auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_index, {VecTy} , {firstOp, secondOp});
-}
-
-Value * IntrinsicCallGenerator::createGatherLoadInstruction(IRBuilder<> &IRB,
-    Value *ptr, Value *predicatedVector, Value *indices) {
-    auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(Intrinsic::aarch64_sve_ld1_gather_sxtw_index,
-        {VecTy}, {predicatedVector, ptr, indices});
-}
-
-
-Value *
-IntrinsicCallGenerator::createLoadInstruction(IRBuilder<> &IRB, Value *ptr,
-                                              Value *predicatedVector) {
+Value *IntrinsicCallGenerator::createLoadInstruction(IRBuilder<> &IRB,
+                                                     Value *ptr,
+                                                     Value *predicatedVector) {
   VectorType *type = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
   // FIXME: Use proper value for Alignment
   return IRB.CreateMaskedLoad(type, ptr, Align(), predicatedVector);
 }
 
-
-void IntrinsicCallGenerator::createScatterStoreInstruction(IRBuilder<> &IRB,
-    Value *elementsVector, Value *ptr, Value *predicatedVector, Value *indices)
-{
-    auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    IRB.CreateIntrinsic(Intrinsic::aarch64_sve_st1_scatter_sxtw, {VecTy},
-        {elementsVector, predicatedVector, ptr, indices});
+void IntrinsicCallGenerator::createScatterStoreInstruction(
+    IRBuilder<> &IRB, Value *elementsVector, Value *ptr,
+    Value *predicatedVector, Value *indices) {
+  auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  IRB.CreateIntrinsic(Intrinsic::aarch64_sve_st1_scatter_sxtw, {VecTy},
+                      {elementsVector, predicatedVector, ptr, indices});
 }
 
-void IntrinsicCallGenerator::createStoreInstruction(IRBuilder<> &IRB, Value *elementsVector,
-    Value *ptr,
-    Value *predicatedVector) {
+void IntrinsicCallGenerator::createStoreInstruction(IRBuilder<> &IRB,
+                                                    Value *elementsVector,
+                                                    Value *ptr,
+                                                    Value *predicatedVector) {
   // FIXME: Use proper value for Alignment
   IRB.CreateMaskedStore(elementsVector, ptr, Align(), predicatedVector);
 }
 
 // TODO: handle double types by changing return type and operands
-Value *
-IntrinsicCallGenerator::createArithmeticInstruction(IRBuilder<> &IRB, unsigned int intrinsic, Value *firstOp,
-                                                    Value *secondOp, Value *predicatedVector) {
-    auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
-    return IRB.CreateIntrinsic(intrinsic, {VecTy}, {predicatedVector, firstOp, secondOp});
+Value *IntrinsicCallGenerator::createArithmeticInstruction(
+    IRBuilder<> &IRB, unsigned int intrinsic, Value *firstOp, Value *secondOp,
+    Value *predicatedVector) {
+  auto *VecTy = VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true);
+  return IRB.CreateIntrinsic(intrinsic, {VecTy},
+                             {predicatedVector, firstOp, secondOp});
 }
 
 Value *IntrinsicCallGenerator::createVscale64Intrinsic(IRBuilder<> &IRB) {
-    return IRB.CreateVScale(IRB.getInt64(VF));
+  return IRB.CreateVScale(IRB.getInt64(VF));
 }
 
 Value *IntrinsicCallGenerator::createVscale32Intrinsic(IRBuilder<> &IRB) {
-    return IRB.CreateVScale(IRB.getInt32(VF));
+  return IRB.CreateVScale(IRB.getInt32(VF));
 }
 
 Value *IntrinsicCallGenerator::createStepVector64Intrinsic(IRBuilder<> &IRB) {
-    return IRB.CreateStepVector(VectorType::get(IRB.getInt64Ty(), VF, /*Scalable*/ true));
+  return IRB.CreateStepVector(
+      VectorType::get(IRB.getInt64Ty(), VF, /*Scalable*/ true));
 }
 
 Value *IntrinsicCallGenerator::createStepVector32Intrinsic(IRBuilder<> &IRB) {
-    return IRB.CreateStepVector(VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true));
+  return IRB.CreateStepVector(
+      VectorType::get(IRB.getInt32Ty(), VF, /*Scalable*/ true));
 }
--- Transformation-Pass/Vectorizer-Pass/IntrinsicCallGenerator/IntrinsicCallGenerator.h
+++ Transformation-Pass/Vectorizer-Pass/IntrinsicCallGenerator/IntrinsicCallGenerator.h
@@ -1,75 +1,79 @@
 
 
+#include "map"
+#include "stack"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/IntrinsicsAArch64.h"
-#include "llvm/Passes/PassPlugin.h"
-#include "llvm/Passes/PassBuilder.h"
 #include "llvm/IR/PassManager.h"
-#include "llvm/Pass.h"
 #include "llvm/IR/ValueMap.h"
-#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Pass.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Passes/PassPlugin.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
-#include "stack"
-#include "map"
+#include "llvm/Transforms/Utils/Cloning.h"
 
 #ifndef IntrinsicCallGenerator_H
 #define IntrinsicCallGenerator_H
 
-
 using namespace llvm;
-
 
 class IntrinsicCallGenerator {
 
 private:
-    int VF;
-    Module *M;
-public:
-    IntrinsicCallGenerator(int VF, Module *M) : VF(VF), M(M) {}
+  int VF;
+  Module *M;
 
 public:
+  IntrinsicCallGenerator(int VF, Module *M) : VF(VF), M(M) {}
 
-    Value *createAllTruePredicates(IRBuilder<> &IRB);
+public:
+  Value *createAllTruePredicates(IRBuilder<> &IRB);
 
-    Value *createCompactInstruction(IRBuilder<> &IRB, Value *toBeCompacted, Value *predicatedVector);
+  Value *createCompactInstruction(IRBuilder<> &IRB, Value *toBeCompacted,
+                                  Value *predicatedVector);
 
-    Value *createCntpInstruction(IRBuilder<> &IRB, Value *elements, Value *predicatedVector);
+  Value *createCntpInstruction(IRBuilder<> &IRB, Value *elements,
+                               Value *predicatedVector);
 
-    Value *createWhileltInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp);
+  Value *createWhileltInstruction(IRBuilder<> &IRB, Value *firstOp,
+                                  Value *secondOp);
 
-    Value *createSpliceInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp,
-                                      Value *predicatedVector);
+  Value *createSpliceInstruction(IRBuilder<> &IRB, Value *firstOp,
+                                 Value *secondOp, Value *predicatedVector);
 
-    Value *createSelInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp,
-                                   Value *predicatedVector);
+  Value *createSelInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp,
+                              Value *predicatedVector);
 
-    Value *createIndexInstruction(IRBuilder<> &IRB, Value *firstOp, Value *secondOp);
+  Value *createIndexInstruction(IRBuilder<> &IRB, Value *firstOp,
+                                Value *secondOp);
 
-    Value *createGatherLoadInstruction(IRBuilder<> &IRB, Value *ptr, Value *predicatedVector,
-                                          Value *indices);
+  Value *createGatherLoadInstruction(IRBuilder<> &IRB, Value *ptr,
+                                     Value *predicatedVector, Value *indices);
 
-    Value *createLoadInstruction(IRBuilder<> &IRB, Value *ptr, Value *predicatedVector);
+  Value *createLoadInstruction(IRBuilder<> &IRB, Value *ptr,
+                               Value *predicatedVector);
 
-    void createScatterStoreInstruction(IRBuilder<> &IRB, Value *elementsVector, Value *ptr,
-                                       Value *predicatedVector, Value *indices);
+  void createScatterStoreInstruction(IRBuilder<> &IRB, Value *elementsVector,
+                                     Value *ptr, Value *predicatedVector,
+                                     Value *indices);
 
-    void createStoreInstruction(IRBuilder<> &IRB, Value *elementsVector, Value *ptr,
-                                Value *predicatedVector);
+  void createStoreInstruction(IRBuilder<> &IRB, Value *elementsVector,
+                              Value *ptr, Value *predicatedVector);
 
-    Value *createArithmeticInstruction(IRBuilder<> &IRB, unsigned int intrinsic, Value *firstOp,
-                                          Value *secondOp, Value *predicatedVector);
+  Value *createArithmeticInstruction(IRBuilder<> &IRB, unsigned int intrinsic,
+                                     Value *firstOp, Value *secondOp,
+                                     Value *predicatedVector);
 
-    Value* createVscale64Intrinsic(IRBuilder<> &IRB);
+  Value *createVscale64Intrinsic(IRBuilder<> &IRB);
 
-    Value* createVscale32Intrinsic(IRBuilder<> &IRB);
+  Value *createVscale32Intrinsic(IRBuilder<> &IRB);
 
-    Value* createStepVector64Intrinsic(IRBuilder<> &IRB);
+  Value *createStepVector64Intrinsic(IRBuilder<> &IRB);
 
-    Value* createStepVector32Intrinsic(IRBuilder<> &IRB);
-
+  Value *createStepVector32Intrinsic(IRBuilder<> &IRB);
 };
 
 #endif
