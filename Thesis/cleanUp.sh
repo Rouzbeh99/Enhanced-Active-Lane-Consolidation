93a32430dbd5156d5905716cc9354bdc51301577
Rouzbeh99 <rouzbehpaktinat99@gmail.com>
2023-06-26 12:16:03 -0600

    Reorder files

---
+++ Thesis/cleanUp.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# pdflatex thesis.txt
+# biber thesis
+# pdflatex thesis.txt
+
+rm *.aux
+rm *.gz
+rm *.log
+rm *.bbl
+rm *.bcf
+rm *.fls
+rm *.lof
+rm *.lot
+rm *.out
+rm *.xml
+rm *.toc
+rm *.blg
+rm *.acn
+rm *.glo
+rm *.ist
